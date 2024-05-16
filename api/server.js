8d7065b90197fabca3b727661c005312edc8759a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-16 11:54:48 -0300

    Reconfigure for use on Vercel

--- api/server.js
+++ api/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Adiciona o middleware para servir arquivos estáticos
 
 let elapsedTime = 0;
 let isRunning = false;
 let timer;
 
-app.get('/timer', (req, res) => {
+app.get('/api/timer', (req, res) => {
   res.json({ elapsedTime, isRunning });
 });
 
-app.post('/start', (req, res) => {
+app.post('/api/start', (req, res) => {
   if (!isRunning) {
     timer = setInterval(() => {
       elapsedTime++;
@@ -26,7 +23,7 @@ app.post('/start', (req, res) => {
   res.json({ message: 'Timer started' });
 });
 
-app.post('/stop', (req, res) => {
+app.post('/api/stop', (req, res) => {
   if (isRunning) {
     clearInterval(timer);
     isRunning = false;
@@ -34,13 +31,11 @@ app.post('/stop', (req, res) => {
   res.json({ message: 'Timer stopped' });
 });
 
-app.post('/reset', (req, res) => {
+app.post('/api/reset', (req, res) => {
   clearInterval(timer);
   elapsedTime = 0;
   isRunning = false;
   res.json({ message: 'Timer reset' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = app;
